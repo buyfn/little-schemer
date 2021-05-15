a1f3c4510f3de89bfb3cbfe2ac742e5258ce55c5
ignat <buyfng@gmail.com>
2021-05-15 14:24:37 +0300

    add multiplication, division and exponentiation

--- 4.rkt
+++ 4.rkt
@@ -29,3 +29,33 @@
       ((null? tup1) null)
       (else (cons (o+ (car tup1) (car tup2))
                   (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define o>
+  (lambda (n m)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (o> (sub1 n) (sub1 m))))))
+
+(define o<
+  (lambda (n m)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (o< (sub1 n) (sub1 m))))))
+
+(define o=
+  (lambda (n m)
+    (not (or (o> n m) (o< n m)))))
+
+(define o**
+  (lambda (n m)
+    (cond
+      ((zero? m) 1)
+      (else (o* n (o** n (sub1 m)))))))
+
+(define o/
+  (lambda (n m)
+    (cond
+      ((< n m) 0)
+      (else (add1 (o/ (o- n m) m))))))
