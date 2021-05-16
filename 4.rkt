ab0d2513758b6117225110ecbfe46a84db5633c5
ignat <buyfng@gmail.com>
2021-05-16 11:32:48 +0300

    complete chapter 4

--- 4.rkt
+++ 4.rkt
@@ -1,61 +1,115 @@
 #lang racket
-(define o+
+(define op+
   (lambda (a b)
     (if (zero? b)
         a
-        (o+ (add1 a) (sub1 b)))))
+        (op+ (add1 a) (sub1 b)))))
 
-(define o-
+(define op-
   (lambda (a b)
     (if (zero? b)
         a
-        (o- (sub1 a) (sub1 b)))))
+        (op- (sub1 a) (sub1 b)))))
 
 (define addtup
   (lambda (tup)
     (cond
       ((null? tup) 0)
-      (else (o+ (car tup) (addtup (cdr tup)))))))
+      (else (op+ (car tup) (addtup (cdr tup)))))))
 
-(define o*
+(define op*
   (lambda (n m)
     (cond
       ((zero? m) 0)
-      (else (o+ n (o* n (sub1 m)))))))
+      (else (op+ n (op* n (sub1 m)))))))
 
 (define tup+
   (lambda (tup1 tup2)
     (cond
       ((null? tup1) null)
-      (else (cons (o+ (car tup1) (car tup2))
+      (else (cons (op+ (car tup1) (car tup2))
                   (tup+ (cdr tup1) (cdr tup2)))))))
 
-(define o>
+(define op>
   (lambda (n m)
     (cond
       ((zero? n) #f)
       ((zero? m) #t)
-      (else (o> (sub1 n) (sub1 m))))))
+      (else (op> (sub1 n) (sub1 m))))))
 
-(define o<
+(define op<
   (lambda (n m)
     (cond
       ((zero? m) #f)
       ((zero? n) #t)
-      (else (o< (sub1 n) (sub1 m))))))
+      (else (op< (sub1 n) (sub1 m))))))
 
-(define o=
+(define op=
   (lambda (n m)
-    (not (or (o> n m) (o< n m)))))
+    (not (or (op> n m) (op< n m)))))
 
-(define o**
+(define op**
   (lambda (n m)
     (cond
       ((zero? m) 1)
-      (else (o* n (o** n (sub1 m)))))))
+      (else (op* n (op** n (sub1 m)))))))
 
-(define o/
+(define op/
   (lambda (n m)
     (cond
       ((< n m) 0)
-      (else (add1 (o/ (o- n m) m))))))
+      (else (add1 (op/ (op- n m) m))))))
+
+(define length
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (length (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((one? n) (cdr lat))
+      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) null)
+      ((number? (car lat)) (no-nums (cdr lat)))
+      (else (cons (car lat) (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) null)
+      ((number? (car lat))
+       (cons (car lat) (all-nums (cdr lat))))
+      (else (all-nums (cdr lat))))))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1) (number? a2)) (= a1 a2))
+      ((and (not (number? a1)) (not (number? a2)))
+       (eq? a1 a2))
+      (else #f))))
+
+(define occur
+  (lambda (a lat)
+    (cond
+      ((null? lat) 0)
+      (else
+       (cond
+         ((eqan? a (car lat))
+          (add1 (occur a (cdr lat))))
+         (else (occur a (cdr lat))))))))
+
+(define one?
+  (lambda (n) (= n 1)))
