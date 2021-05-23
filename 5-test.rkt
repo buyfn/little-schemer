4d4a9c0196cfbfd31c565baf994443d7b3856d81
ignat <buyfng@gmail.com>
2021-05-23 12:51:15 +0300

    implement insertR*

--- 5-test.rkt
+++ 5-test.rkt
@@ -2,9 +2,27 @@
 
 (require rackunit "5.rkt")
 
-(define l1 '((coffee) cup ((tea) cup) (and (hick)) cup))
-(define l2 '(((tomato sauce)) ((bean) sauce) (and ((flying)) sauce)))
-(define l3 '(a b c))
-(check-equal? (rember* 'cup l1) '((coffee) ((tea)) (and (hick))))
-(check-equal? (rember* 'sauce l2) '(((tomato)) ((bean)) (and ((flying)))))
-(check-equal? (rember* 'b l3) '(a c))
+(test-case
+    "rember*"
+  (define l1 '((coffee) cup ((tea) cup) (and (hick)) cup))
+  (define l2 '(((tomato sauce)) ((bean) sauce) (and ((flying)) sauce)))
+  (define l3 '(a b c))
+  (check-equal? (rember* 'cup l1) '((coffee) ((tea)) (and (hick))))
+  (check-equal? (rember* 'sauce l2) '(((tomato)) ((bean)) (and ((flying)))))
+  (check-equal? (rember* 'b l3) '(a c)))
+
+(test-case
+    "insertR*"
+  (define l '((how much (wood))
+              could
+              ((a (wood) chuck))
+              (((chuck)))
+              (if (a) ((wood chuck)))
+              could chuck wood))
+  (check-equal? (insertR* 'roast 'chuck l)
+                '((how much (wood))
+                  could
+                  ((a (wood) chuck roast))
+                  (((chuck roast)))
+                  (if (a) ((wood chuck roast)))
+                  could chuck roast wood)))
--- 5.rkt
+++ 5.rkt
@@ -12,5 +12,17 @@
          (else (cons (car l) (rember* a (cdr l))))))
       (else (cons (rember* a (car l))
                   (rember* a (cdr l)))))))
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+      ((null? l) null)
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old)
+          (cons old (cons new (insertR* new old (cdr l)))))
+         (else (cons (car l) (insertR* new old (cdr l))))))
+      (else (cons (insertR* new old (car l))
+                  (insertR* new old (cdr l)))))))
                 
-(provide rember*)
+(provide rember* insertR*)
