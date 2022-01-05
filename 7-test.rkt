bfeb31e73f929d6d9bfcfc3aa750a366794ed2e4
ignat <buyfng@gmail.com>
2022-01-05 22:28:11 +0200

    implement subset?

--- 7-test.rkt
+++ 7-test.rkt
@@ -20,3 +20,12 @@
  (check-equal? (makeset lat1) '(apple peach pear plum lemon))
  (check-equal? (makeset lat2) '(apple 3 pear 4 9))
 )
+
+(test-case
+ "subset?"
+ (define set1 '(5 chicken wings))
+ (define set2 '(5 hamburgers 2 pieces fried chicken and light duckling wings))
+ (define set3 '(4 pounds of horseradish))
+ (define set4 '(four pounds chicken and 5 ounces horseradish))
+ (check-equal? (subset? set1 set2) #t)
+ (check-equal? (subset? set3 set4) #f))
--- 7.rkt
+++ 7.rkt
@@ -18,4 +18,11 @@
       (else
        (cons (car lat) (makeset (cdr lat)))))))
 
-(provide set? makeset)
+(define subset?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #t)
+      (else (and (member (car set1) set2)
+                 (subset? (cdr set1) set2))))))
+
+(provide set? makeset subset?)
